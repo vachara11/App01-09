14f6a4d439102595b453d9044762c331aa6e2073
Watchara Ketkaew <wk996110@gmail.com>
2024-07-24 14:29:39 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonRow, IonGrid, IonItem, IonCol, IonLabel, IonIcon, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonIcon, IonLabel, IonCol, IonItem, IonGrid, IonRow, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   constructor() {}
